0f948804825f509b8be64f147493148c85d7a92b
Rifat <nk.rifat47@gmail.com>
2026-02-02 21:56:07 +0600

    fixed 2

--- src/app/modules/payment/payment.route.js
+++ src/app/modules/payment/payment.route.js
@@ -6,7 +6,11 @@ import { Role } from "../../utils/role.js";
 const router = Router();
 
 router.post("/webhook", paymentController.handleSamcartWebhook);
-router.get("/verify", checkAuthMiddleware(Role.STUDENT, Role.ADMIN, Role.OWNER), paymentController.verifyPayment);
-router.get("/checkout/:planKey", checkAuthMiddleware(Role.STUDENT, Role.ADMIN, Role.OWNER), paymentController.initiatePurchase);
+router.get(
+  "/verify",
+  checkAuthMiddleware(Role.STUDENT, Role.ADMIN, Role.OWNER),
+  paymentController.verifyPayment,
+);
+router.get("/checkout/:planKey", paymentController.initiatePurchase);
 
 export const PaymentRoutes = router;
